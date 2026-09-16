1d3df257029806ace1a87e6d2c501d0083fa86c2
Kyra Kay Tomaquin <167772031+wawmatcha@users.noreply.github.com>
2026-09-16 01:50:10 +0100

    Add Movie Catalog machine problem

---
+++ Movie.java
@@ -0,0 +1,14 @@
+public class Movie{
+ 
+ String title;
+ String genre;
+ int duration;
+ 
+ 
+ void displayInfo(){
+ System.out.println(title + ", " + genre + ", " + duration + " minutes");
+ }
+
+
+}
+ 
---
+++ main.java
@@ -0,0 +1,33 @@
+public class main {
+
+  public static void main (String [] args){
+  
+  Movie s1 = new Movie();
+  
+  s1.title = "Avatar: Fire and Ash";
+  s1.genre = "Scifi";
+  s1.duration =197;
+  
+ Movie s2 = new Movie();
+  
+  s2.title = "Obsession";
+  s2.genre = "Thriller";
+  s2.duration = 109;
+
+
+ Movie s3 = new Movie();
+  
+  s3.title = "Spider-Man: Brand New Day";
+  s3.genre = "Action Adventure, Scifi";
+  s3.duration = 145;
+
+
+s1.displayInfo();
+s2.displayInfo();
+s3.displayInfo();
+  
+  
+  
+  }
+
+}
